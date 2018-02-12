a1656bcd9225e9215b6a56373e8d2d1afbbe3af1
Khai Do <zaro0508@gmail.com>
2018-02-12 14:06:50 -0800

    Setup CI to update sophos utm infrastructure
    The sophos utm was created on aws admincentral account using sophos
    CF template (autoscaling.template).  This will allow the CI to keep
    the sophos infrastructure resources updated.

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -68,3 +68,37 @@ else
   echo $message
 fi
 
+# must manually create an EC2 key pair (awsKeyName) before creating sophos-utm stack
+STACK_NAME="sophos-utm"
+CF_TEMPLATE="autoscaling.template"
+echo -e "\nDeploying CF template $CF_BUCKET_URL/$CF_TEMPLATE"
+UPDATE_CMD="aws cloudformation update-stack \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--notification-arns $CloudformationNotifyLambdaTopicArn \
+--template-url $CF_BUCKET_URL/$CF_TEMPLATE \
+--parameters \
+ParameterKey=awsLicenseType,ParameterValue="Hourly" \
+ParameterKey=awsAvailabilityZone1,ParameterValue="us-east-1a" \
+ParameterKey=awsAvailabilityZone2,ParameterValue="us-east-1d" \
+ParameterKey=awsNetworkPrefix,ParameterValue=\"VpcSubnetPrefix\" \
+ParameterKey=awsTrustedNetwork,ParameterValue=\"$FhcrcVpnCidrip\" \
+ParameterKey=basicHostname,ParameterValue="sophosutm" \
+ParameterKey=basicAdminEmail,ParameterValue=\"$OperatorEmail\" \
+ParameterKey=basicAdminPassword,ParameterValue=\"$SophosInitAdminPassword\" \
+ParameterKey=basicOrganization,ParameterValue="Sage-Bionetworks" \
+ParameterKey=basicCity,ParameterValue="Seattle" \
+ParameterKey=basicCountry,ParameterValue="United States" \
+ParameterKey=awsKeyName,ParameterValue="sophosutm" \
+ParameterKey=debugMode,ParameterValue="on"
+message=$($UPDATE_CMD 2>&1 1>/dev/null)
+error_code=$(echo $?)
+if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+  echo -e "\nNo stack changes detected. An update is not required."
+  error_code=0
+elif [[ $error_code -ne 0 ]]; then
+  echo $message
+  exit $error_code
+else
+  echo $message
+fi
