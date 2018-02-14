ce93f25386cb4a8f77e624d62846705ec30a7f3d
Khai Do <zaro0508@gmail.com>
2018-02-13 16:02:17 -0800

    Add sophos utm template
    We needed to add exports to sophos UTM template to allow our Active
    directory template to reference the VPC. A PR[1] was made to upstream
    project.  We add the template here for now but may change to reference
    upstream later.  This template is forked from ver 9.506 [2].
    
    The sophos template is larger than 51K so we also need to change the
    deployment to upload templates to S3 bucket before validating them.
    
    [1] https://github.com/sophos-iaas/aws-cf-templates/pull/21
    [2] https://github.com/sophos-iaas/aws-cf-templates/blob/master/utm/9.506/autoscaling.template

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-FILES=cf_templates/*
-for f in $FILES
+# Need to upload TEMPLATES to S3 before validating due to template-body MAX 512K length
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/validate-template.html#options
+S3_BUCKET="bootstrap-awss3cloudformationbucket-19qromfd235z9"
+S3_BUCKET_URL="s3://$S3_BUCKET/$TRAVIS_BRANCH/"
+TEMP_DIR="temp"
+TEMPLATES=cf_templates/*
+
+# Validate files in the temporary folder then move them to release folder if no errors
+for f in $TEMPLATES
 do
-  echo -e "\nValidating CF template $f"
-  aws cloudformation validate-template --template-body file://$f
+  echo -e "\nUploading cf_templates to $S3_BUCKET_URL/$TEMP_DIR/$f"
+  aws s3 cp $f $S3_BUCKET_URL/$TEMP_DIR/$f
 done
 
-S3_TARGET_DIR="s3://bootstrap-awss3cloudformationbucket-19qromfd235z9/$TRAVIS_BRANCH/"
-echo -e "\nUploading cf_templates to $S3_TARGET_DIR"
-aws s3 cp --recursive cf_templates $S3_TARGET_DIR
+TEMPLATE_URL="https://$S3_BUCKET/$TRAVIS_BRANCH/"
+for f in $TEMPLATES
+do
+  echo -e "\nValidating CF template $TEMPLATE_URL/$TEMP_DIR/$f"
+  aws cloudformation validate-template --template-url $TEMPLATE_URL/$TEMP_DIR/$f
+  aws s3 mv $S3_BUCKET_URL/$TEMP_DIR/$f $S3_BUCKET_URL/$f
+done
