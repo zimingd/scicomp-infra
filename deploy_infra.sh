66122193d46780b3a3c54fbaae47c1ae4edd77c7
Khai Do <zaro0508@gmail.com>
2018-03-15 13:26:25 -0700

    Cleanup and setup for CI
    Cleanup copied files from admincentral-infra.
    
    * Merge CI fixes from admincentral
    * Created initial "peer-vpn-vpc" stack on scicomp
    * Setup repo to update CF stacks on scicomp account

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 # Need to upload TEMPLATES to S3 before validating due to template-body MAX 51K length
 # https://docs.aws.amazon.com/cli/latest/reference/cloudformation/validate-template.html#options
 S3_BUCKET="bootstrap-awss3cloudformationbucket-19qromfd235z9"
-S3_BUCKET_URL="s3://$S3_BUCKET/$TRAVIS_BRANCH"
+S3_BUCKET_PATH="scicomp-infra/$TRAVIS_BRANCH"
+S3_BUCKET_URL="s3://$S3_BUCKET/$S3_BUCKET_PATH"
 TEMP_DIR="temp"
 
 # get list of templates
@@ -16,13 +18,17 @@ do
   aws s3 cp $f $S3_BUCKET_URL/$TEMP_DIR/$f
 done
 
-TEMPLATE_URL="https://s3.amazonaws.com/$S3_BUCKET/$TRAVIS_BRANCH"
+TEMPLATE_URL="https://s3.amazonaws.com/$S3_BUCKET/$S3_BUCKET_PATH/$TEMP_DIR"
 for f in $TEMPLATES
 do
-  echo -e "\nValidating CF template $TEMPLATE_URL/$TEMP_DIR/$f"
-  aws cloudformation validate-template --template-url $TEMPLATE_URL/$TEMP_DIR/$f
+  echo -e "\nValidating CF template $TEMPLATE_URL/$f"
+  aws cloudformation validate-template --template-url $TEMPLATE_URL/$f
+done
+
+for f in $TEMPLATES
+do
+  echo -e "\nPromote CF template to https://s3.amazonaws.com/$S3_BUCKET/$S3_BUCKET_PATH/$f"
   aws s3 mv $S3_BUCKET_URL/$TEMP_DIR/$f $S3_BUCKET_URL/$f
 done
 
 popd
-
--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-CF_BUCKET_URL="https://s3.amazonaws.com/bootstrap-awss3cloudformationbucket-19qromfd235z9/master"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+CF_BUCKET_URL="https://s3.amazonaws.com/bootstrap-awss3cloudformationbucket-19qromfd235z9"
 
 STACK_NAME="bootstrap"
 CF_TEMPLATE="$STACK_NAME.yml"
@@ -9,7 +10,7 @@ UPDATE_CMD="aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --capabilities CAPABILITY_NAMED_IAM \
 --notification-arns $CloudformationNotifyLambdaTopicArn \
---template-url $CF_BUCKET_URL/$CF_TEMPLATE"
+--template-url $CF_BUCKET_URL/aws-infra/master/$CF_TEMPLATE"
 # Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
 message=$($UPDATE_CMD 2>&1 1>/dev/null)
 error_code=$(echo $?)
@@ -30,32 +31,11 @@ UPDATE_CMD="aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --capabilities CAPABILITY_NAMED_IAM \
 --notification-arns $CloudformationNotifyLambdaTopicArn \
---template-url $CF_BUCKET_URL/$CF_TEMPLATE \
+--template-url $CF_BUCKET_URL/aws-infra/master/$CF_TEMPLATE \
 --parameters \
 ParameterKey=FhcrcVpnCidrip,ParameterValue=\"$FhcrcVpnCidrip\" \
-ParameterKey=OperatorEmail,ParameterValue=\"$OperatorEmail\""
-message=$($UPDATE_CMD 2>&1 1>/dev/null)
-error_code=$(echo $?)
-if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
-  echo -e "\nNo stack changes detected. An update is not required."
-  error_code=0
-elif [[ $error_code -ne 0 ]]; then
-  echo $message
-  exit $error_code
-else
-  echo $message
-fi
-
-STACK_NAME="vpc"
-CF_TEMPLATE="$STACK_NAME.yml"
-echo -e "\nDeploying CF template $CF_BUCKET_URL/$CF_TEMPLATE"
-UPDATE_CMD="aws cloudformation update-stack \
---stack-name $STACK_NAME \
---capabilities CAPABILITY_NAMED_IAM \
---notification-arns $CloudformationNotifyLambdaTopicArn \
---template-url $CF_BUCKET_URL/$CF_TEMPLATE \
---parameters \
-ParameterKey=VpcSubnetPrefix,ParameterValue=\"$VpcSubnetPrefix\""
+ParameterKey=OperatorEmail,ParameterValue=\"$OperatorEmail\" \
+ParameterKey=VpcPeeringRequesterAwsAccountId,ParameterValue=\"$AdmincentralAwsAccountId\""
 message=$($UPDATE_CMD 2>&1 1>/dev/null)
 error_code=$(echo $?)
 if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
@@ -90,28 +70,17 @@ else
   echo $message
 fi
 
-# must manually create an EC2 key pair (awsKeyName) before creating sophos-utm stack
-STACK_NAME="sophos-utm"
-CF_TEMPLATE="autoscaling.template"
+STACK_NAME="vpc"
+CF_TEMPLATE="$STACK_NAME.yml"
 echo -e "\nDeploying CF template $CF_BUCKET_URL/$CF_TEMPLATE"
 UPDATE_CMD="aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --capabilities CAPABILITY_NAMED_IAM \
 --notification-arns $CloudformationNotifyLambdaTopicArn \
---template-url $CF_BUCKET_URL/$CF_TEMPLATE \
+--template-url $CF_BUCKET_URL/aws-infra/master/$CF_TEMPLATE \
 --parameters \
-ParameterKey=awsLicenseType,ParameterValue="Hourly" \
-ParameterKey=awsAvailabilityZone1,ParameterValue="us-east-1a" \
-ParameterKey=awsAvailabilityZone2,ParameterValue="us-east-1d" \
-ParameterKey=awsNetworkPrefix,ParameterValue=\"$SophosVpcSubnetPrefix\" \
-ParameterKey=awsTrustedNetwork,ParameterValue=\"$FhcrcVpnCidrip\" \
-ParameterKey=basicHostname,ParameterValue="sophosutm" \
-ParameterKey=basicAdminEmail,ParameterValue=\"$OperatorEmail\" \
-ParameterKey=basicAdminPassword,ParameterValue=\"$SophosInitAdminPassword\" \
-ParameterKey=basicOrganization,ParameterValue="Sage-Bionetworks" \
-ParameterKey=basicCity,ParameterValue="Seattle" \
-ParameterKey=awsKeyName,ParameterValue="sophosutm" \
-ParameterKey=debugMode,ParameterValue="on""
+ParameterKey=VpcName,ParameterValue=\"$STACK_NAME\" \
+ParameterKey=VpcSubnetPrefix,ParameterValue="10.5""
 message=$($UPDATE_CMD 2>&1 1>/dev/null)
 error_code=$(echo $?)
 if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
@@ -123,3 +92,29 @@ elif [[ $error_code -ne 0 ]]; then
 else
   echo $message
 fi
+
+STACK_NAME="peer-vpn-vpc"
+CF_TEMPLATE="peer-route-config.yml"
+echo -e "\nDeploying CF template cf_templates/$CF_TEMPLATE"
+UPDATE_CMD="aws cloudformation update-stack \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--notification-arns $CloudformationNotifyLambdaTopicArn \
+--template-url $CF_BUCKET_URL/aws-infra/master/$CF_TEMPLATE \
+--parameters \
+ParameterKey=PeeringConnectionId,ParameterValue="pcx-0013f868" \
+ParameterKey=VpcPrivateRouteTable,ParameterValue="rtb-a5c01ed9" \
+ParameterKey=VpcPublicRouteTable,ParameterValue="rtb-08c01e74" \
+ParameterKey=VpnCidr,ParameterValue="10.1.0.0/16""
+message=$($UPDATE_CMD 2>&1 1>/dev/null)
+error_code=$(echo $?)
+if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+  echo -e "\nNo stack changes detected. An update is not required."
+  error_code=0
+elif [[ $error_code -ne 0 ]]; then
+  echo $message
+  exit $error_code
+else
+  echo $message
+fi
+
