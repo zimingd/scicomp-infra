bcb83de1d3e00fc9e03a5e107d9d6c725c1b001f
Khai Do <zaro0508@gmail.com>
2018-03-22 19:18:47 -0700

    Lambda to cleanup jumpcloud systems
    A resource (i.e. EC2 instance) is provisioned with a CF stack and it
    gets registered as a "system" in jumpcloud.  When the resource is removed
    from AWS (via a stack delete) it will continue to be registered on jumpcloud.
    
    We add this lambda to periodically clean up those registered systems on
    jumpcloud.

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -119,3 +119,24 @@ else
   echo $message
 fi
 
+STACK_NAME="jc-clean-systems"
+CF_TEMPLATE="jumpcloud.yml"
+echo -e "\nUpdating $STACK_NAME with template cf_templates/$CF_TEMPLATE"
+UPDATE_CMD="aws cloudformation update-stack \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--notification-arns $CloudformationNotifyLambdaTopicArn \
+--template-body file://cf_templates/$CF_TEMPLATE \
+--parameters \
+ParameterKey=JcServiceApiKey,ParameterValue=\"$JcServiceApiKey\""
+message=$($UPDATE_CMD 2>&1 1>/dev/null)
+error_code=$(echo $?)
+if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
+  echo -e "\nNo stack changes detected. An update is not required."
+  error_code=0
+elif [[ $error_code -ne 0 ]]; then
+  echo $message
+  exit $error_code
+else
+  echo $message
+fi
