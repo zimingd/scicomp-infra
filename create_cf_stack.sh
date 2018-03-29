6acd58b4689980cdb44a66552b990df2da591df2
Khai Do <zaro0508@gmail.com>
2018-03-29 14:55:54 -0700

    refactor provision script with a function

--- create_cf_stack.sh
+++ create_cf_stack.sh
@@ -2,8 +2,34 @@
 COMMITTER_EMAIL="$(git log -2 $TRAVIS_COMMIT --pretty="%cE"|grep -v -m1 noreply@github.com)"
 AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 
-# EC2 instance for demo
-STACK_NAME="ec2-test2"
+function provision {
+  local provision_cmd=$1
+  local message=$($provision_cmd 2>&1 1>/dev/null)
+  local error_code=$(echo $?)
+  if [[ $error_code -ne 0 && $message =~ .*"AlreadyExistsException".* ]]; then
+    error_code=0
+  elif [[ $error_code -ne 0 ]]; then
+    echo $message
+    exit $error_code
+  else
+    echo -e "\nCreating stack $STACK_NAME with template cf_templates/$CF_TEMPLATE ..."
+    aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+    status_code=$(echo $?)
+    if [[ status_code -eq 255 ]]; then
+      echo -e "\nFailed getting status of $STACK_NAME"
+      exit $status_code
+    else
+      echo -e "\nSending provisioned resource info..."
+      local EC2_IP="$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[0].Outputs[0].OutputValue')"
+      aws ses send-email --to "$COMMITTER_EMAIL" --subject "Scicomp Automated Provisioning" \
+      --text "An EC2 instance has been provisioned for you. To connect to this resource, login to the Sage VPN then type \"ssh <YOUR_JUMPCLOUD_USERNAME>@$EC2_IP\" (i.e. ssh jsmith@$EC2_IP)" \
+      --from "$OperatorEmail"
+    fi
+  fi
+}
+
+# Provision the following resources
+STACK_NAME="ec2-test3"
 DEPARTMENT="Platform"
 PROJECT="Infrastructure"
 INSTANCE_TYPE="t2.nano"
@@ -25,25 +51,4 @@ ParameterKey=InstanceType,ParameterValue=\"$INSTANCE_TYPE\" \
 ParameterKey=JcServiceApiKey,ParameterValue=\"$JcServiceApiKey\" \
 ParameterKey=JcSystemsGroupId,ParameterValue=\"$JcSystemsGroupId\" \
 ParameterKey=JcConnectKey,ParameterValue=\"$JcConnectKey\""
-message=$($CLI_CMD 2>&1 1>/dev/null)
-error_code=$(echo $?)
-if [[ $error_code -ne 0 && $message =~ .*"AlreadyExistsException".* ]]; then
-  error_code=0
-elif [[ $error_code -ne 0 ]]; then
-  echo $message
-  exit $error_code
-else
-  echo -e "\nCreating stack $STACK_NAME with template cf_templates/$CF_TEMPLATE"
-  aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
-  status_code=$(echo $?)
-  if [[ status_code -eq 255 ]]; then
-    echo -e "\nFailed getting status of $STACK_NAME"
-    exit $status_code
-  else
-    echo -e "\nSending provisioned resource info..."
-    EC2_IP="$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[0].Outputs[0].OutputValue')"
-    aws ses send-email --to "$COMMITTER_EMAIL" --subject "Scicomp Automated Provisioning" \
-    --text "An EC2 instance has been provisioned for you. To connect to this resource, login to the Sage VPN then type \"ssh <YOUR_JUMPCLOUD_USERNAME>@$EC2_IP\" (i.e. ssh jsmith@$EC2_IP)" \
-    --from "$OperatorEmail"
-  fi
-fi
+provision "$CLI_CMD"
