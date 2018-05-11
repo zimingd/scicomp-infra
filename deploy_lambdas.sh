f5351944730e6fe0ce5e70181ecdf21a8abe9854
Khai Do <zaro0508@gmail.com>
2018-05-11 09:41:23 -0700

    update the lambda root folder

--- deploy_lambdas.sh
+++ deploy_lambdas.sh
@@ -5,15 +5,16 @@ set -e
 S3_BUCKET="essentials-awss3lambdaartifactsbucket-1ef8sqdil160e"
 S3_BUCKET_PATH="scicomp-infra/$TRAVIS_BRANCH"
 
-LAMBDA_DIRS=$(ls -d lambdas/*)
+LAMBDA_ROOT_DIR=lambdas
+LAMBDA_DIRS=$(ls -d $LAMBDA_ROOT_DIR/*)
 for lambda_dir in $LAMBDA_DIRS
 do
   lambda_name="${lambda_dir##*/}"
   lambda_package="${lambda_name}.zip"
   echo -e "\nPackaging lambda ${lambda_name}"
-  pushd lambda/${lambda_name}
+  pushd $LAMBDA_ROOT_DIR/${lambda_name}
   zip -r ../${lambda_package} *
   popd
   echo -e "\nUploading lambda package to s3://$S3_BUCKET/$S3_BUCKET_PATH/${lambda_package}"
-  aws s3 cp lambda/${lambda_package} s3://$S3_BUCKET/$S3_BUCKET_PATH/${lambda_package}
+  aws s3 cp $LAMBDA_ROOT_DIR/${lambda_package} s3://$S3_BUCKET/$S3_BUCKET_PATH/${lambda_package}
 done
