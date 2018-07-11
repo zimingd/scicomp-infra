1a16f6f4adce8f8fb54bd1a2e20fd67bff2d1ff4
Khai Do <zaro0508@gmail.com>
2018-07-10 19:38:22 -0700

    add hook to setup synapse external bucket
    * Sceptre hook to setup external bucket which will replace the
      SynapseExternalBucketHelper.sh script.
    * Instead of searching in git log for committer email just have
      the user specify the email (OwnerEmail) to send the bucket info to.
      Relying on the email in git log can be error prone, people may not
      even setup an email in their git config.
    * Make SynapseExternalBucketHelper.sh script compatible until it gets
      removed
    * Update existing synapse bucket deployments with OwnerEmail parameter

--- auto-provision/SynapseExternalBucketHelper.sh
+++ auto-provision/SynapseExternalBucketHelper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+# !!!!  This script is deprecated, use the scepter synapse_external_bucket hook instead !!!!
 
 # Run after execution of SynapseExternalBucket.yaml template
 
@@ -7,11 +8,11 @@ set -e
 STACK_NAME=$1
 SYNAPSE_BUCKET_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-$STACK_NAME-SynapseExternalBucket'].Value" --output text)
 SYNAPSE_USER_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-$STACK_NAME-SynapseUserName'].Value" --output text)
+OWNER_EMAIL=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-$STACK_NAME-OwnerEmail'].Value" --output text)
 echo "$SYNAPSE_USER_NAME" > owner.txt
 aws s3 cp owner.txt s3://$SYNAPSE_BUCKET_NAME/
 
 # Send email to the bucket owner
-COMMITTER_EMAIL="$(git log -2 $TRAVIS_COMMIT --pretty="%cE"|grep -v -m1 noreply@github.com)"
-aws ses send-email --to "$COMMITTER_EMAIL" --subject "Scicomp Automated Provisioning" \
+aws ses send-email --to "$OWNER_EMAIL" --subject "Scicomp Automated Provisioning" \
 --text "An S3 bucket has been provisioned on your behalf. The bucket name is $SYNAPSE_BUCKET_NAME" \
 --from "aws.scicomp@sagebase.org"
